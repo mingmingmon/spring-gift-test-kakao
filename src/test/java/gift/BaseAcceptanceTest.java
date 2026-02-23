225ab0060acba105504a61ca9a96d4d0e1805dac
mingmingmon <mingmingmon@naver.com>
2026-02-23 18:02:20 +0900

    feat(profile): Spring 프로파일로 테스트/개발 DB 분리

--- src/test/java/gift/BaseAcceptanceTest.java
+++ src/test/java/gift/BaseAcceptanceTest.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public abstract class BaseAcceptanceTest {
 
     @LocalServerPort
--- src/test/java/gift/cucumber/CucumberSpringConfig.java
+++ src/test/java/gift/cucumber/CucumberSpringConfig.java
@@ -4,9 +4,11 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CucumberSpringConfig {
 
 	@LocalServerPort
