7ea35140e93058f90c18084c907f128d081b518e
mingmingmon <mingmingmon@naver.com>
2026-02-25 18:22:58 +0900

    feat(DatabaseCleaner): JpaRepository와 @Transactional을 활용한 DB 정리 방식으로 변경

--- src/test/java/gift/cucumber/DatabaseCleanUp.java
+++ src/test/java/gift/cucumber/DatabaseCleanUp.java
@@ -1,46 +1,15 @@
 package gift.cucumber;
 
 import io.cucumber.java.Before;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Table;
-import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.List;
 
 public class DatabaseCleanUp {
 
 	@Autowired
-	JdbcTemplate jdbcTemplate;
-
-	@Autowired
-	EntityManager entityManager;
+	private DatabaseCleaner databaseCleaner;
 
 	@Before(order = 0)
 	public void cleanUp() {
-		List<String> tableNames = entityManager.getMetamodel().getEntities().stream()
-			.map(EntityType::getJavaType)
-			.filter(clazz -> clazz.isAnnotationPresent(Entity.class))
-			.map(clazz -> {
-				Table table = clazz.getAnnotation(Table.class);
-				if (table != null && !table.name().isEmpty()) {
-					return table.name();
-				}
-				return convertToSnakeCase(clazz.getSimpleName());
-			})
-			.toList();
-
-		if (!tableNames.isEmpty()) {
-			String truncateSql = "TRUNCATE TABLE " +
-				String.join(", ", tableNames) +
-				" CASCADE";
-			jdbcTemplate.execute(truncateSql);
-		}
-	}
-
-	private String convertToSnakeCase(String name) {
-		return name.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
+		databaseCleaner.execute();
 	}
 }
