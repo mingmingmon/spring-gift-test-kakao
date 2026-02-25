f98a509dfeb7f7e35eba7c0653e72aca78cfe5af
mingmingmon <mingmingmon@naver.com>
2026-02-25 17:18:15 +0900

    fix(SharedContext): ScenarioScope 도입

--- src/test/java/gift/cucumber/steps/SharedContext.java
+++ src/test/java/gift/cucumber/steps/SharedContext.java
@@ -1,9 +1,11 @@
 package gift.cucumber.steps;
 
+import io.cucumber.spring.ScenarioScope;
 import io.restassured.response.Response;
 import org.springframework.stereotype.Component;
 
 @Component
+@ScenarioScope
 public class SharedContext {
 
 	private Response lastResponse;
